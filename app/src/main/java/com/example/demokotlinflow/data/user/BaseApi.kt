c18b8ea6045e0fc4410b97c30a27a34d56fc6d05
ManaliTatva <manali.bhalodia@tatvasoft.com>
2022-05-12 12:14:48 +0530

    init login api

--- app/src/main/java/com/example/demokotlinflow/data/user/BaseApi.kt
+++ app/src/main/java/com/example/demokotlinflow/data/user/BaseApi.kt
@@ -4,10 +4,14 @@ import com.example.demokotlinflow.data.user.remote.response.UserListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
 interface BaseApi {
     @GET("api/users")
     suspend fun getAllUser(
         @Query("offset") offset: Int,
         @Query("limit") limit: Int
     ): UserListResponse
+
+//    @POST("api/v1/verify_cus")
+//    fun verifyCustomer(@Body loginRequest: LoginRequest?): Single<LoginModel?>?
 }
