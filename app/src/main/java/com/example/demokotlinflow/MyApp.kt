e4dfa8eed51e895b7a904fdee499486111f47af4
ManaliTatva <manali.bhalodia@tatvasoft.com>
2022-07-22 10:40:13 +0530

    Revert "App center sdk setup"
    This reverts commit 6372c4b70369d996b0038e5d02d392bd4d06b7cf.

--- app/src/main/java/com/example/demokotlinflow/MyApp.kt
+++ app/src/main/java/com/example/demokotlinflow/MyApp.kt
@@ -1,12 +1,7 @@
 package com.example.demokotlinflow
 
 import android.app.Application
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.internal.Contexts.getApplication
-
 
 @HiltAndroidApp
 class MyApp:Application() {
@@ -17,9 +12,5 @@ class MyApp:Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        AppCenter.start(
-            getApplication(instance), "df0031e7-89b8-4841-82b6-31547cd6a6b5",
-            Analytics::class.java, Crashes::class.java
-        )
     }
 }
